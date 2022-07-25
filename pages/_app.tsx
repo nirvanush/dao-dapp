78a084e4339bf89b00d1e61f2cb2f2f3a9c0e23f
nirvanush <nirvanushergo@gmail.com>
2022-07-25 11:49:30 +0300

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '../pages/components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <header>
-
-      </header>
+    <ChakraProvider>
+      <Navbar />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
