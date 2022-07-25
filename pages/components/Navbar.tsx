c3a65b1d1918ee95189551fa51e369103b19257e
nirvanush <nirvanushergo@gmail.com>
2022-07-25 12:27:21 +0300

    more cleanup

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -2,29 +2,18 @@ import {
   Box,
   Flex,
   HStack,
-  IconButton,
   Button,
-  useDisclosure,
   useColorModeValue,
 } from '@chakra-ui/react';
 
-import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+import { AddIcon } from '@chakra-ui/icons';
 
 
 export default function withAction() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
           <HStack spacing={8} alignItems={'center'}>
             <Box>SigmaValley DAO</Box>
           </HStack>
